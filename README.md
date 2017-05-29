a60072f79b37b446508868aa751c83b77cdbbaa5
vslutov <vslutov@yandex.ru>
2017-05-29 15:42:26 +0300

    Add license

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # hockey
-search hockey player coords by video
+classify hockey player team by image
+
+## LICENSE
+
+    ----------------------------------------------------------------------------
+    "THE BEER-WARE LICENSE" (Revision 42):
+    <vslutov@yandex.ru> wrote this file.   As long as you retain this notice you
+    can do whatever you want with this stuff. If we meet some day, and you think
+    this stuff is worth it, you can buy me a beer in return.      Vladimir Lutov
+    ----------------------------------------------------------------------------
+
+## Documentation
+
+А нет ее, читайте код ;-)
--- classifying/validate.py
+++ classifying/validate.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# ----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+# <vslutov@yandex.ru> wrote this file.   As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return.      Vladimir Lutov
+# ----------------------------------------------------------------------------
+
 import numpy as np
 from matplotlib import pyplot as plt
 from sklearn import neighbors, cross_validation, svm, ensemble, metrics
